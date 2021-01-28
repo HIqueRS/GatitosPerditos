8eb5952c810d29d2bc6c20c5dd48860296458109
Hique <riquesouzalima11@gmail.com>
2021-01-28 18:00:10 -0300

    tentando o input novo

---
+++ GatitosPerditos/Assets/Scripts/Movement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    private float side;
+
+    void Start()
+    {
+        side = 10;
+    }
+    
+    void Update()
+    {
+        Debug.Log( string.Concat(side,gameObject.name ));
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        side = context.ReadValue<float>();
+    }
+
+
+
+}
