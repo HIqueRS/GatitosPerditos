86eb131efd4ab19f883f4ea7c7cef295d3a7ca7b
Aline Pessoa da Silva Kaiper <lilica.kaiper@gmail.com>
2021-01-29 20:37:01 -0300

    Arrumando a geração de mapa

--- GatitosPerditos/Assets/Scripts/RoomGenerator.cs
+++ GatitosPerditos/Assets/Scripts/RoomGenerator.cs
@@ -12,13 +12,15 @@ public class RoomGenerator : MonoBehaviour
 	[SerializeField]
 	private float alturaSala, larguraSala;
 
-    private int[,] matriz;
+	[SerializeField]
+	private int altura, largura;
 
-	private int xMeio = 2;
-	private int yMeio = 2;
+	private int[,] matriz;
 
-	private int largura = 5;
-	private int altura = 5;
+	private int xMeio;
+	private int yMeio;
+
+	
 
 	private Vector3 transformSala;
 
@@ -26,17 +28,64 @@ public class RoomGenerator : MonoBehaviour
 
     void Start()
     {
+		xMeio = altura / 2;
+		yMeio = largura / 2;
+
         matriz = new int[altura, largura];
 
-        for (int i = 0; i < altura; i++)
+        for (int i = 0; i < largura; i++)
         {
-            for (int j = 0; j < largura; j++)
+			matriz[0, i] = 0;
+			matriz[altura - 1, i] = 0;
+		}
+		for (int i = 0; i < altura; i++)
+        {
+			matriz[i, 0] = 0;
+			matriz[i, largura - 1] = 0;
+            
+        }
+		
+		for (int i = 1; i < altura - 1; i++)
+        {
+            for (int j = 1; j < largura - 1; j++)
             {
                 matriz[i, j] = -1;
             }
         }
 
         RandTile(xMeio, yMeio);
+
+		for(int i = 0; i < altura; i++)
+		{
+			for(int j = 0; j < largura; j++)
+			{
+				switch(matriz[i , j])
+				{
+					case 0:
+						InstanciaSala(i, j, 0);
+						break;
+					case 1:
+						InstanciaSala(i, j, 1);
+						break;
+					case 2:
+						InstanciaSala(i, j, 2);
+						break;
+					case 3:
+						InstanciaSala(i, j, 3);
+						break;
+					case 4:
+						InstanciaSala(i, j, 4);
+						break;
+					case 5:
+						InstanciaSala(i, j, 5);
+						break;
+					default:
+						break;
+				}
+					
+			}
+		}
+
     }
 
     private void RandTile(int linha, int coluna)
@@ -56,38 +105,26 @@ public class RoomGenerator : MonoBehaviour
 		if (numero >= 0 && numero <= 40)
 		{
 			matriz[linha, coluna] = 0;
-
-			InstanciaSala(linha, coluna, 0);
 		}
 		else if (numero > 40 && numero <= 72)
 		{
 			matriz[linha, coluna] = 1;
-
-			InstanciaSala(linha, coluna, 1);
 		}
 		else if (numero > 72 && numero <= 104)
 		{
 			matriz[linha, coluna] = 2;
-
-			InstanciaSala(linha, coluna, 2);
 		}
 		else if (numero > 104 && numero <= 136)
 		{
 			matriz[linha, coluna] = 3;
-
-			InstanciaSala(linha, coluna, 3);
 		}
 		else if (numero > 136 && numero <= 168)
 		{
 			matriz[linha, coluna] = 4;
-
-			InstanciaSala(linha, coluna, 4);
 		}
 		else
 		{
 			matriz[linha, coluna] = 5;
-
-			InstanciaSala(linha, coluna, 5);
 		}
 
 
@@ -127,8 +164,8 @@ public class RoomGenerator : MonoBehaviour
 	
 	private void InstanciaSala(int linha, int coluna, int sala)
 	{
-		transformSala.x = 0 - (larguraSala * linha);
-		transformSala.y = 0+ (alturaSala * coluna);
+		transformSala.x = 0 + (larguraSala * coluna);
+		transformSala.y = 0 - (alturaSala * linha);
 		transformSala.z = 0;
 
 		GameObject.Instantiate(salas[sala], transformSala, Quaternion.identity);
