75e9a72ad328fb486a9d84106b3f17ecc9472d39
Hique <riquesouzalima11@gmail.com>
2021-01-29 00:38:52 -0300

    inputs working

---
+++ GatitosPerditos/Assets/Scripts/MovementTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementTest : MonoBehaviour
+{
+    [SerializeField]
+    private InputSchema control;
+    private float dir;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dir = Input.GetAxis(control.axis);
+
+        transform.position += new Vector3(dir * Time.deltaTime, 0,0);
+    }
+
+   
+}
