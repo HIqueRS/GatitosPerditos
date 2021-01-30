0a5b348758856fa37f053343ecf8b5c786781eb1
Aline Pessoa da Silva Kaiper <lilica.kaiper@gmail.com>
2021-01-29 22:53:53 -0300

    só salvando

--- GatitosPerditos/Assets/Scripts/RoomGenerator.cs
+++ GatitosPerditos/Assets/Scripts/RoomGenerator.cs
@@ -20,7 +20,7 @@ public class RoomGenerator : MonoBehaviour
 	private int xMeio;
 	private int yMeio;
 
-	
+	private int salasViaveis;
 
 	private Vector3 transformSala;
 
@@ -28,32 +28,20 @@ public class RoomGenerator : MonoBehaviour
 
     void Start()
     {
+		salasViaveis = 0;
 		xMeio = altura / 2;
 		yMeio = largura / 2;
 
         matriz = new int[altura, largura];
 
-        for (int i = 0; i < largura; i++)
-        {
-			matriz[0, i] = 0;
-			matriz[altura - 1, i] = 0;
+		while(salasViaveis < 30)
+		{
+			salasViaveis = 0;
+			IniciarMatriz();
+			RandTile(xMeio, yMeio);
+			VerificaNumeroSalas();
+			Debug.Log(salasViaveis);
 		}
-		for (int i = 0; i < altura; i++)
-        {
-			matriz[i, 0] = 0;
-			matriz[i, largura - 1] = 0;
-            
-        }
-		
-		for (int i = 1; i < altura - 1; i++)
-        {
-            for (int j = 1; j < largura - 1; j++)
-            {
-                matriz[i, j] = -1;
-            }
-        }
-
-        RandTile(xMeio, yMeio);
 
 		for(int i = 0; i < altura; i++)
 		{
@@ -169,6 +157,39 @@ public class RoomGenerator : MonoBehaviour
 		transformSala.z = 0;
 
 		GameObject.Instantiate(salas[sala], transformSala, Quaternion.identity);
+	}
+
+	private void VerificaNumeroSalas()
+	{
+		foreach (int numeroSala  in matriz)
+		{
+			if (numeroSala >= 1 && numeroSala <= 5)
+			{
+				salasViaveis++;
+			}
+		}
+	}
+
+	private void IniciarMatriz()
+	{
+		for (int i = 0; i < largura; i++)
+		{
+			matriz[0, i] = 0;
+			matriz[altura - 1, i] = 0;
+		}
+		for (int i = 0; i < altura; i++)
+		{
+			matriz[i, 0] = 0;
+			matriz[i, largura - 1] = 0;
+
+		}
+		for (int i = 1; i < altura - 1; i++)
+		{
+			for (int j = 1; j < largura - 1; j++)
+			{
+				matriz[i, j] = -1;
+			}
+		}
 	}
 }
 
